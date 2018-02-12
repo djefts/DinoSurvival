5dc45e1c2c4833a07884df56a0611a5db8fb55a4
David <dvdjefts27@gmail.com>
2018-02-12 16:40:47 -0500

    Added dino class stats

--- src/Dinosaur.java
+++ src/Dinosaur.java
@@ -1,2 +1,16 @@
 public class Dinosaur {
+    private int health;
+    private int speed;
+    private int attack;
+    private int defense;
+    private int foodStorage;
+    private int waterStorage;
+    private int kids;
+    private int foodToReproduce;
+    private int turnsWithoutFood;
+    private int maxAge;
+    private boolean herd;
+    private boolean territorial;
+    private boolean herbivore;
+
 }
