b508abd05577c4bc2e26ed4d4e6a9065baedb0f0
David <dvdjefts27@gmail.com>
2018-02-22 13:32:32 -0500

    Revert "Still working on .gitignore"
    This reverts commit 9d694a9a2f44637399ebace55523a06146652508.

--- src/SimulationManager.java
+++ src/SimulationManager.java
@@ -3,11 +3,11 @@ public class SimulationManager {
     private final int minSimLength = 500;
     private final int maxSimLength = 5000;
     private int simulationLength;
-    private Object[][] thePlayingField;
+    private Object[][] thePlayingField;//asdf
     
     public void loop() {
         setSimulationLength();
-        System.out.println("Simulation will run for " + getSimulationLength() + "loops.");
+        System.out.println(getSimulationLength());
         
         do {
             
