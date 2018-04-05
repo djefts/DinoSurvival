49a4fd2c59c181f1c4dae456368171439623d631
ranftc <casey.ranft@gmail.com>
2018-04-04 20:10:16 -0400

    Added a method to print array. Also added a text file version of the Database

--- src/ReadDino.java
+++ src/ReadDino.java
@@ -3,6 +3,8 @@ import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import sun.security.util.Length;
+
 public class ReadDino
 {
 	public static void main (String []args) 
@@ -12,8 +14,9 @@ public class ReadDino
 		ArrayList<ArrayList<String>> arrayOfDinos = new ArrayList<>();
 
 		//Open csv file
-		String fileName = "DinoTable.csv";
-//		String fileName = "DinoTable.txt";
+		//		String fileName = "DinoTable.csv";
+		String fileName = "DinoTable.txt";
+		//String fileName = "/resources/DinoTable.txt";
 		File file = new File(fileName);
 		ReadDino readDino = new ReadDino();
 
@@ -24,13 +27,16 @@ public class ReadDino
 			input.close();
 
 		} catch (FileNotFoundException e) {
-
+			System.out.println("Apparently the file wasn't found");
 		}
+		
+		//readDino.printArray(arrayOfDinos);
 	}
-	
+
 	private ArrayList<ArrayList<String>> processData(Scanner input,  ArrayList<ArrayList<String>> arrayOfDinos)
 	{
 		int index = 0;
+		int totalColumns = 0;
 		while(input.hasNext())
 		{
 			String line = input.next();
@@ -39,12 +45,32 @@ public class ReadDino
 			for(String s : stats)
 			{
 				arrayOfDinos.get(index).add(s);
+				totalColumns++;
 			}
 			//update row or dinosaur we're on
 			index++;
 		}
 		return arrayOfDinos;
 
+	}
+	private void printArray(ArrayList<ArrayList<String>> arrayOfDinos)
+	{
+		//prints first row
+		for (int i = 0; i < 14; i++ )
+		{
+			System.out.println(arrayOfDinos.get(i));
+		}
+		//print Dinos with their stats
+		for (int row = 1; row < 12; row++ )
+		{
+			
+			for (int column = 0; column < 14; column++ )
+			{
+				System.out.println(arrayOfDinos.get(row).get(column));
+			}
+		}
+
+		return;
 	}
 
 }
