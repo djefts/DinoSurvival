e948b4448af09974624d2852bf519cb750d0f007
ranftc <casey.ranft@gmail.com>
2018-04-09 15:15:22 -0400

    Updated GUI

--- src/GUI.java
+++ src/GUI.java
@@ -6,7 +6,7 @@
  *Date:
  *
  *Variable List: 
- *
+ * 
  *Methods List: 
  **********************************************************/
 
@@ -36,74 +36,7 @@ import javafx.geometry.Side;
 import javafx.geometry.Insets;
 
 
-@SuppressWarnings("ALL")
 public class GUI extends Application {
-<<<<<<< HEAD
-    
-    //Define window properties
-    private final int Height = 300;
-    private final int Width = 450;
-    private final boolean Resizeable = false; //To prevent user from resizing window
-    
-    //Stage
-    @Override
-    public void start(Stage primaryStage) {
-        
-        //Create border pane
-        BorderPane showSim = new BorderPane();
-        
-        //BorderPane design
-        Label showYN;
-        showYN = new Label("Would you like to see a dinosaur simulation?");
-        showYN.setStyle("-fx-font: 20 pt");
-        
-        Button yesButt, noButt;
-        yesButt = new Button("Fuck yeah!");
-        yesButt.setStyle("-fx-font: 10 pt");
-        yesButt.setMinSize(200, 200);
-        
-        noButt = new Button("Hell no!");
-        noButt.setStyle("-fx-font: 10 pt");
-        noButt.setMinSize(200, 200);
-        
-        HBox centerButts = new HBox();
-        centerButts.setAlignment(Pos.CENTER);
-        centerButts.setSpacing(15);
-        centerButts.getChildren().addAll(yesButt, noButt);
-        
-        
-        //Add to BorderPane
-        showSim.setTop(showYN); //Top
-        showSim.setAlignment(showYN, Pos.CENTER);
-        
-        showSim.setCenter(centerButts); //Center
-        showSim.setAlignment(centerButts, Pos.TOP_RIGHT);
-        
-        /***********************Event Handlers**************************/
-        //INSERT EVENT HANDLER HERE
-        
-        
-        /***************************************************************/
-        
-        
-        //create scene with all objects in it
-        Scene scene = new Scene(showSim, Width, Height, Color.GRAY);
-        
-        //Set stage properties
-        primaryStage.setTitle("Dinosaur Survival Simulator");
-        primaryStage.setResizable(Resizeable);
-        primaryStage.setScene(scene);
-        primaryStage.setHeight(Height);
-        primaryStage.setWidth(Width);
-        primaryStage.show();
-        
-    }
-    
-    public static void main(String[] args) {
-        Application.launch(args);
-    }
-    
-=======
 	
 	//Define window properties
 	public int Height = 300 ;
@@ -138,9 +71,9 @@ public class GUI extends Application {
 		//Set stage properties
 		primaryStage.setTitle("Dinosaur Survival Simulator");
 		primaryStage.setResizable(Resizeable);
-		primaryStage.setScene(sceneOne); 
-		primaryStage.setHeight(HeightA);
-		primaryStage.setWidth(WidthA);		
+		primaryStage.setScene(sceneTwo); 
+		primaryStage.setHeight(Height);
+		primaryStage.setWidth(Width);		
 		primaryStage.show();
 	      
 	}
@@ -149,12 +82,7 @@ public class GUI extends Application {
 	public static void main(String[] args) {
 		 Application.launch(args);	
 	}
- 
-
 	
-	
-	
->>>>>>> Christian's-Branch-(DONT-TOUCH-MY-SHIT,-DAVID)
 }
 
 
