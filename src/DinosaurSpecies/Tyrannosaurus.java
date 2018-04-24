e7ca9283023c0689e24b5605e78abd016bfed937
ranftc <casey.ranft@gmail.com>
2018-04-24 09:35:09 -0400

    referenced Array Test

--- src/DinosaurSpecies/Tyrannosaurus.java
+++ src/DinosaurSpecies/Tyrannosaurus.java
@@ -5,19 +5,19 @@ public class Tyrannosaurus extends Dinosaur {
     public Tyrannosaurus(int xLoc, int yLoc) {
         ReadDino readDino = new ReadDino();
         ArrayList<String> data = readDino.individualDino("Tyrannosaurus");
-        addHealth(4);
-        setSpeed(3);
-        setAttack(7);
-        setDefense(2);
-        setFoodStorage(8);
-        setWaterStorage(4);
-        setKids(1);
-        setFoodToReproduce(6);
-        setTurnsWithoutFood(4);
-        setMaxAge(4);
-        setHerbivore(0);
-        setHerd(0);
-        setTerritorial(1);
+        addHealth(data.get(0));
+        setSpeed(data.get(1));
+        setAttack(data.get(2));
+        setDefense(data.get(3));
+        setFoodStorage(data.get(4));
+        setWaterStorage(data.get(5));
+        setKids(data.get(6));
+        setFoodToReproduce(data.get(7));
+        setTurnsWithoutFood(data.get(8));
+        setMaxAge(data.get(9));
+        setHerbivore(data.get(10));
+        setHerd(data.get(11));
+        setTerritorial(data.get(12));
         setDinosaur(xLoc, yLoc);
     }
 }
